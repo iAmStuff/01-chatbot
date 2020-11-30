ccbf09590f99ce458cc41191afec6c52b2cf3273
Quentin Sothern <quentinsothern@developersinstitute.co.nz>
2020-12-01 11:37:49 +1300

    added userName framework

--- Submission/static/js/script.js
+++ Submission/static/js/script.js
@@ -1,124 +1,126 @@
 // Remember the name
 let userName = "";
-// placeholder userName
-userName = "John Doe";
 
 // Populate discussionTree object
-const discussionTree = {
-  question: `Hi ${userName}, first things first: Are you new to anime?`,
-  questionType: "newbie",
-  yes: {
-    question:
-      "Would you rather watch romantic comedy style shows, or maybe something more action-y?",
-    questionType: "genre",
-    romCom: {
+const getDiscussionTree = () => {
+  // question: is the text the bot will print when it reaches that point in the tree
+  // questionType: is a value which has corresponding key in the knownInputs object
+  // answers are any objects after these first two keys
+  // answers can be sub objects, in the event that you intend to lead the conversation to another question
+  // or answers can be arrays, in the event you wish to present the user with a list of anime that meets the criteria
+  // endpoint arrays should contain strings consisting of anime titles, this may change in the future to be objects that better describe the anime
+  return {
+    question: `Hi ${userName}, first things first: Are you new to anime?`,
+    questionType: "newbie",
+    yes: {
       question:
-        "Do you want to watch something lightweight, or maybe something more heavy going?",
-      questionType: "weight",
-      light: {
+        "Would you rather watch romantic comedy style shows, or maybe something more action-y?",
+      questionType: "genre",
+      romCom: {
+        question:
+          "Do you want to watch something lightweight, or maybe something more heavy going?",
+        questionType: "weight",
+        light: {
+          question: `Do you want to watch one of the "must-watch" classics, or would you rather watch something more recent?`,
+          questionType: "filmAge",
+          old: [
+            "Toradora",
+            "Sakurasou no Pet na Kanojo",
+            "Mikakunin de Shinkoukei",
+            "Hyouka",
+          ],
+          new: {
+            question:
+              "Do you want to watch a currently airing anime? Or would you rather watch something that has finished airing?",
+            questionType: "airing",
+            ongoing: ["Tonikaku Kawaii"],
+            finished: ["Kaguya-sama: Love is war"],
+          },
+        },
+        heavy: [
+          "Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai",
+          "Ano Hi Mita Hana no Namae wo Bokutachi wa Mada Shiranai (Anohana)",
+          "Shigatsu wa Kimi no Uso",
+          "Steins; Gate",
+        ],
+      },
+      action: {
         question: `Do you want to watch one of the "must-watch" classics, or would you rather watch something more recent?`,
         questionType: "filmAge",
-        old: [
-          "Toradora",
-          "Sakurasou no Pet na Kanojo",
-          "Mikakunin de Shinkoukei",
-          "Hyouka",
-        ],
+        old: ["Tengen Toppa Gurren Lagann", "Noragami", "Fullmetal alchemist"],
         new: {
           question:
-            "Do you wnat to watch a currently airing anime? Or would you rather watch something that has finished airing?",
+            "Do you want to watch a currently airing anime? Or would you rather watch something that has finished airing?",
           questionType: "airing",
-          ongoing: ["Tonikaku Kawaii"],
-          finished: ["Kaguya-sama: Love is war"],
+          ongoing: ["Jujutsu Kaisen"],
+          finished: ["One Punch Man", "Demon slayer: Kimetsu no Yaiba"],
         },
       },
-      heavy: [
-        "Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai",
-        "Ano Hi Mita Hana no Namae wo Bokutachi wa Mada Shiranai (Anohana)",
-        "Shigatsu wa Kimi no Uso",
-        "Steins; Gate",
-      ],
     },
-    action: {
-      question: `Do you want to watch one of the "must-watch" classics, or would you rather watch something more recent?`,
-      questionType: "filmAge",
-      old: ["Tengen Toppa Gurren Lagann", "Noragami", "Fullmetal alchemist"],
-      new: {
+    no: {
+      question:
+        "Are you in a Romcom kinda mood? Or maybe an action kinda mood instead?",
+      questionType: "genre",
+      romCom: {
+        question:
+          "Do you want to watch something lightweight, or maybe something more heavy going?",
+        questionType: "weight",
+        light: {
+          question:
+            "Do you want to watch a currently airing anime? Or would you rather watch something that has finished airing?",
+          questionType: "airing",
+          ongoing: ["Majou no Tabitabi"],
+          finished: [
+            "Sewayaki Kitsune no Senko-san",
+            "5-Toubun no Hanayome",
+            "Wotaku ni Koi wa Muzukashii",
+            "Yuru Camp△",
+          ],
+        },
+        heavy: {
+          question: "Do you feel like suffering today?",
+          questionType: "suffering",
+          yes: ["Clannad", "Kanojo Okarishimasu"],
+          no: {
+            question:
+              "Do you want to watch a currently airing anime? Or would you rather watch something that has finished airing?",
+            questionType: "airing",
+            ongoing: ["Kamisama ni Natta Hi"],
+            finished: [
+              "Haruhi Suzumiya",
+              "Monogatari series",
+              "Yahari Ore no Seishun Love Comedy wa Machigatteiru",
+            ],
+          },
+        },
+      },
+      action: {
         question:
           "Do you want to watch a currently airing anime? Or would you rather watch something that has finished airing?",
         questionType: "airing",
-        ongoing: ["Jujutsu Kaisen"],
-        finished: ["One Punch Man", "Demon slayer: Kimetsu no Yaiba"],
-      },
-    },
-  },
-  no: {
-    question:
-      "Are you in a Romcom kinda mood? Or maybe an action kinda mood instead?",
-    questionType: "genre",
-    romCom: {
-      question:
-        "Do you want to watch something lightweight, or maybe something more heavy going?",
-      questionType: "weight",
-      light: {
-        question:
-          "Do you wnat to watch a currently airing anime? Or would you rather watch something that has finished airing?",
-        questionType: "airing",
-        ongoing: ["Majou no Tabitabi"],
-        finished: [
-          "Sewayaki Kitsune no Senko-san",
-          "5-Toubun no Hanayome",
-          "Wotaku ni Koi wa Muzukashii",
-          "Yuru Camp△",
-        ],
-      },
-      heavy: {
-        question: "Do you feel like suffering today?",
-        questionType: "suffering",
-        yes: ["Clannad", "Kanojo Okarishimasu"],
-        no: {
-          question:
-            "Do you wnat to watch a currently airing anime? Or would you rather watch something that has finished airing?",
-          questionType: "airing",
-          ongoing: ["Kamisama ni Natta Hi"],
+        ongoing: {
+          question: "Opinon on horror anime?",
+          questionType: "horror",
+          yes: ["Higurashi no Naku Koro ni Gou"],
+          no: ["Jujutsu Kaisen"],
           finished: [
-            "Haruhi Suzumiya",
-            "Monogatari series",
-            "Yahari Ore no Seishun Love Comedy wa Machigatteiru",
+            "Demon slayer: Kimetsu no Yaiba",
+            "Tengen Toppa Gurren Lagann",
+            "Noragami",
+            "Fullmetal Alchemist",
           ],
         },
       },
     },
-    action: {
-      question:
-        "Do you wnat to watch a currently airing anime? Or would you rather watch something that has finished airing?",
-      questionType: "airing",
-      ongoing: {
-        question: "Opinon on horror anime?",
-        questionType: "horror",
-        yes: ["Higurashi no Naku Koro ni Gou"],
-        no: ["Jujutsu Kaisen"],
-        finished: [
-          "Demon slayer: Kimetsu no Yaiba",
-          "Tengen Toppa Gurren Lagann",
-          "Noragami",
-          "Fullmetal Alchemist",
-        ],
-      },
-    },
-  },
+  };
 };
-
-// clone discussionTree to a variable so we can move the tree root
-let currentBranch = discussionTree;
 
 // declare path on global scope so we can modify it from our nested functions below
 let path = "";
 
 // knownInputs object here
 // inputs are kinda vague and unnatural at this state
-// things should improve once get a chance to work on the
-// validate input function.
+// things should improve once i get a chance to work on the validate input function.
 const knownInputs = {
   // keywords that contain spaces currently cannot be validated with this method. revisit this later
   horror: {
@@ -165,6 +167,15 @@ const knownInputs = {
 // array to track the chat messages
 const chatLogs = [];
 
+const setName = (input) => {
+  console.log(`setting name to ${input}`);
+  userName = input;
+  currentBranch = getDiscussionTree();
+  return userName;
+};
+
+// clone discussionTree to a variable so we can move the tree root
+let currentBranch;
 // write an input validation function here
 const validateInput = (input, type) => {
   console.log("begin validateInput");
@@ -191,27 +202,31 @@ const validateInput = (input, type) => {
 
 // write a conversation reset function here
 
+// main
 const getBotReply = (msg) => {
-  // validate input
-  type = currentBranch.questionType;
-  validateInput(msg, type);
-  // rewrite currentBranch with new root
-  currentBranch = currentBranch[path];
-  // if statement to check for global commands. eg. restart, name change or whatever
-  // if statement to see if we found the answer yet
-  if (currentBranch.length == 1) {
-    console.log("answer is an array with a single entry");
-    return `I suggest you try watching ${currentBranch[0]}`;
-  } else if (Array.isArray(currentBranch)) {
-    console.log("answer is array");
-    return `I suggest you try watching ${
-      currentBranch[Math.floor(Math.random() * currentBranch.length)]
-      // random entry from answers array
-    }`;
+  console.log("begin getBotReply");
+  // first check for any special requests. eg empty username, dark mode...
+  if (userName == "") {
+    setName(msg);
+  } else {
+    // validate input
+    type = currentBranch.questionType;
+    validateInput(msg, type);
+    // rewrite currentBranch with new root
+    currentBranch = currentBranch[path];
+    // if statement to check for global commands. eg. restart, name change or whatever goes here
+    // if statement to see if we found the answer yet
+    if (currentBranch.length == 1) {
+      console.log("answer is an array with a single entry");
+      return `I suggest you try watching ${currentBranch[0]}`;
+    } else if (Array.isArray(currentBranch)) {
+      console.log("answer is array");
+      return `I suggest you try watching ${
+        currentBranch[Math.floor(Math.random() * currentBranch.length)]
+      }`; // random entry from answers array;
+    }
   }
-  // TODO: ask for userName
-
-  return currentBranch.question;
+  return currentBranch.question; // print next response to screen
 };
 
 const renderChatbox = () => {
